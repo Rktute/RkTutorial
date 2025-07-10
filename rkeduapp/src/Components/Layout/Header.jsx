7ea713ed82e02a08a794033bb9ff5d85343c6612
Deepak <Rjtutorial.official@gmail.com>
2025-07-10 17:37:59 +0530

    add header file and add css

--- rkeduapp/src/Components/Layout/Header.jsx
+++ rkeduapp/src/Components/Layout/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import "../Layout/Header.css";
+import rklogo from "../../Assets/Rk.jpeg"
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <>
+      <header className='headerMain'>
+        <div className='logo'>
+          <img src={rklogo} alt="" className='logo img ' />
+        </div>
+        <nav className='nav'>
+          <a href='/' className='nav-link'>Home</a>
+          <a href="/about" className="nav-link">About</a>
+          <a href="/contact" className="nav-link">Contact</a>
+        </nav>
+      </header>
+    </>
   )
 }
 
