0bb3d8e92bf39f5b4e0c36c270d9c053c7f865bd
Deepak <Rjtutorial.official@gmail.com>
2025-07-14 22:52:14 +0530

    @media query

--- rkeduapp/src/Components/Layout/Header.jsx
+++ rkeduapp/src/Components/Layout/Header.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../Layout/Header.css";
 import rklogo from "../../Assets/Rk.jpeg";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className='headerMain'>
       <div className='logo'>
         <img src={rklogo} alt="RK Logo" className='logo-img' />
       </div>
 
-      <nav className='nav'>
+      {/* Hamburger icon */}
+      <div className='hamburger' onClick={() => setMenuOpen(!menuOpen)}>
+        <span className='bar'></span>
+        <span className='bar'></span>
+        <span className='bar'></span>
+      </div>
+
+      {/* Nav Links */}
+      <nav className={`nav ${menuOpen ? 'open' : ''}`}>
         <a href='/' className='nav-link'>Home</a>
         <a href='/about' className='nav-link'>About</a>
         <a href='/contact' className='nav-link'>Contact</a>
         <a href='/blog' className='nav-link'>Blog</a>
         <a href='/service' className='nav-link'>Service</a>
         <a href='/notes' className='nav-link'>Notes</a>
-                <a href='/Students' className='nav-link'>Students</a>
-
+        <a href='/Students' className='nav-link'>Students</a>
       </nav>
 
+      {/* Search */}
       <div className='search-container'>
         <input type="text" placeholder="Search..." className='search-input' />
         <button className='search-btn'>🔍</button>
